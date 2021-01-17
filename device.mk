01a7bcc55d892b0948f7ae94d628a902b39678fb
Deivid21 <david.parra.ignacio@gmail.com>
2021-01-17 14:06:32 -0300

    channel: update thermal conf from QPYS30.52-22-12
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -73,8 +73,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-channel-NA_TMO.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-channel-NA_TMO.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-channel.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-channel.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Inherit from motorola sdm632-common
 $(call inherit-product, device/motorola/sdm632-common/common.mk)
